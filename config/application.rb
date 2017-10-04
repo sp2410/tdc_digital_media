6376178530374c7a36fcabb9bf573a03847b7e1b
saprakas@syr.edu <saprakas@syr.edu>
2017-10-04 16:42:11 -0700

    fixing blog

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Tdcdigitalmedia
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.precompile += Ckeditor.assets
-	config.assets.precompile += %w( ckeditor/* )
+	config.assets.precompile += %w(#{config.root}/app/assets/javascript/ckeditor/* )
 	config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
     # Settings in config/environments/* take precedence over those specified here.
